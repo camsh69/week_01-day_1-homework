ece222b8caec7fd3c2c093c4ab5f8f7d6a1bc486
Campbell Forsyth <camsh69@gmail.com>
2021-11-15 18:47:22 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,8 @@
+age = input("Guess my age? ")
+
+if age == "49":
+    print("Well done! I know I don't look it :-)")
+elif age < "49":
+    print("You guessed too low!")
+else:
+    print("You guessed too high!")
